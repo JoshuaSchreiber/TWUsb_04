f25cf89709c902023c37acd02b3fde2d4bd632df
BSG322397 <joshua.schreiber.bg@bs-gn.de>
2024-11-04 12:07:24 +0100

    EWF

--- src/main/java/MindConnection.java
+++ src/main/java/MindConnection.java
@@ -58,72 +58,118 @@ public class MindConnection {
             i++;
             Thread.sleep(10);
         }
+        System.out.println("Left Loop");
         return data;
     }
     public static ArrayList<Integer>  receiving() throws TWUsbException, InterruptedException {
         if(!receivingConnection()) return null;
+        System.out.println("        => Connection Complete");
         ArrayList<Integer> data = new ArrayList<>();
+        boolean stop = false;
         if(ReadAllDigital() == 2){
-            while(ReadAllDigital() != 0){
+            System.out.println("Gotten 01 as Transition Start");
+            while(!stop){
                 data.add(ReadAllDigital());
+                if( data.get(data.size() - 8) == 0 && data.get(data.size() - 7) == 0 && data.get(data.size() - 6) == 0 && data.get(data.size() - 5) == 0 && data.get(data.size() - 4) == 0 && data.get(data.size() - 3) == 0 && data.get(data.size() - 1) == 1 && data.get(data.size() - 2) == 1){
+                    for(int i = 0; i < 8; i++){
+                        data.remove(data.size() - 1);
+                    }
+                    System.out.println("Gotten transition end 11");
+                    stop = true;
+                }
             }
         }
+        System.out.println("Gotten data: " + data);
         return data;
     }
 
     public static boolean senden(String data) throws TWUsbException, InterruptedException {
         if(!creatingConnection()) return false;
+        System.out.println("        => Transmitting Data");
         char[] dataListChar = data.toCharArray();
         int[] dataListInt = new int[dataListChar.length];
         String[] dataListString = new String[dataListChar.length];
+
+        System.out.println("        => Giving Data Go (Value2)");
         TWUsb.WriteAllDigital(2);
+
+        String completeData = "";
         for(int i = 0; i < dataListChar.length; i++){
             dataListInt[i] = (int) dataListChar[i];
             dataListString[i] = Integer.toBinaryString(dataListInt[i]);
 
             dataListString[i] =  String.format("%8s", Integer.toBinaryString(dataListInt[i])).replace(' ', '0');
-            oneBitOut(dataListString[i]);
+            completeData = completeData + dataListString[i];
         }
-        TWUsb.WriteAllDigital(3);
+        System.out.println("Complete Data: " + completeData);
+        char[] completeDataChar = completeData.toCharArray();
+        for(int i = 0; i < completeDataChar.length; i++){
+            writeOneBinary(completeDataChar[i]);
+        }
+
+        System.out.println();
+
+        System.out.println("        => Transmition complete");
+        writeOneBinary('0');
+        writeOneBinary('0');
+        writeOneBinary('0');
+        writeOneBinary('0');
+        writeOneBinary('0');
+        writeOneBinary('0');
+        writeOneBinary('1');
+        writeOneBinary('1');
+        System.out.println("Written Transmittion End");
 
         return true;
     }
 
 
-    public static void oneBitOut(String oneBit) throws TWUsbException {
-        char[] charArray = oneBit.toCharArray();
-        for(int i = 0; i < charArray.length; i++){
-            writeOneBinary(String.valueOf(charArray[i]));
-        }
-    }
-    public static void writeOneBinary(String binary) throws TWUsbException {
-        if(binary.equals("1")) TWUsb.WriteAllDigital(1);
+
+    public static void writeOneBinary(Character binary) throws TWUsbException {
+        if(binary.equals('1')) TWUsb.WriteAllDigital(1);
         else TWUsb.WriteAllDigital(0);
+        System.out.print("Written:" + binary + " ");
     }
 
     public static boolean creatingConnection() throws TWUsbException, InterruptedException {
+        System.out.println("        => Creating Connection");
         TWUsb.WriteAllDigital(2); // DTR
+        System.out.println("Sent DTR");
         if(!received(2)) return false; // DSR
+        System.out.println("Received DSR");
 
         TWUsb.WriteAllDigital(4);
+        System.out.println("Sent DCD");
         if(!received(4)) return false;
+        System.out.println("Received Träger");
 
         TWUsb.WriteAllDigital(8);
+        System.out.println("Sent CTS");
         if(!received(8)) return false;
+        System.out.println("Received RTS");
 
+        System.out.println("        => Connection Complete");
         return true;
     }
 
     public static boolean receivingConnection() throws TWUsbException, InterruptedException {
+        System.out.println("        => Receiving Connection");
         if(!received(2)) return false; // DSR
+        System.out.println("Received DSR");
         TWUsb.WriteAllDigital(2); // DTR
+        System.out.println("Sent DTR");
 
         if(!received(4)) return false;
+        System.out.println("Received Träger");
         TWUsb.WriteAllDigital(4);
+        System.out.println("Sent DCD");
 
         if(!received(8)) return false;
+        System.out.println("Received RTS");
         TWUsb.WriteAllDigital(8);
+        System.out.println("Sent CTS");
 
+        System.out.println("        => Connection Complete");
         return true;
     }
 
