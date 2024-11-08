9b608fa882cc4b9945aa052c6fe61698ce141635
BSG322397 <joshua.schreiber.bg@bs-gn.de>
2024-11-08 11:20:17 +0100

    EWF

--- src/main/java/Senden.java
+++ src/main/java/Senden.java
@@ -1,5 +1,6 @@
 import de.wenzlaff.twusb.schnittstelle.exception.TWUsbException;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Senden extends Thread {
@@ -10,14 +11,26 @@ public class Senden extends Thread {
         while (true) {
             Scanner scanner = new Scanner(System.in);
             String data = scanner.nextLine();
-            sendData = data;
-            try {
-                sendConnection.senden(sendData);
-            } catch (TWUsbException e) {
-                throw new RuntimeException(e);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
+
+            ArrayList<Character> chars = new ArrayList<Character>();
+            for (char c : data.toCharArray()) {
+                chars.add(c);
             }
+            if(chars.get(0) == 'W' && chars.get(1) == 'r' && chars.get(2) == 'i' &&chars.get(3) == 't' && chars.get(4) == 'e' && chars.get(5) == ':'){
+                data = "";
+                for(int i = 6; i < chars.size(); i++){
+                    data += chars.get(i);
+                }
+                sendData = data;
+                try {
+                    sendConnection.senden(sendData);
+                } catch (TWUsbException e) {
+                    throw new RuntimeException(e);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
         }
     }
 }
